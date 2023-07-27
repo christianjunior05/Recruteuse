abccb45adf10fcc9f3133442991bc0d8b9af04f5
F0f979 <iamfofana21@gmail.com>
2023-07-27 16:25:25 +0000

    modif

--- src/app/profil-c/profil-c.component.ts
+++ src/app/profil-c/profil-c.component.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profil-c.component.css']
 })
 export class ProfilCComponent implements OnInit {
+  
+  bmodif=true
+  bregist=true
 
   constructor() { }
 
   ngOnInit(): void {
+    document.getElementById('bmodif').addEventListener('click', () => {
+      if (this.bmodif) {
+        for (let i = 0; i < document.getElementsByClassName('modif').length; i++) {
+          document.getElementsByClassName('modif').item(i).setAttribute('class', 'nmodif');        
+          document.getElementsByClassName('nmodif').item(i).setAttribute('class', 'modif');
+          document.getElementById('bregist').style.display="block";      
+          document.getElementById('bmodif').style.display="none";      
+        }  
+      }
+    });
+
+    document.getElementById('bregist').addEventListener('click', () => {
+      if (this.bmodif) {
+        for (let i = 0; i < document.getElementsByClassName('modif').length; i++) {
+          document.getElementsByClassName('nmodif').item(i).setAttribute('class', 'modif');        
+          document.getElementsByClassName('modif').item(i).setAttribute('class', 'nmodif');
+          document.getElementById('bregist').style.display="none";      
+          document.getElementById('bmodif').style.display="block";      
+        }  
+      }
+    })
   }
 
 }
